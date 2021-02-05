3696ad5ae6a180d65f6dda95e777250af0f2af5f
Tim McC <tim@carpe-dmvpn.com>
2021-02-05 04:21:08 -0800

    Finished Day 25 - added file save

--- Day25/main-states-game.py
+++ Day25/main-states-game.py
@@ -14,7 +14,8 @@ states_data = pandas.read_csv("50_states.csv")
 print(states_data)
 state_list = states_data["state"].to_list()
 
-while number_correct < 50:
+game_running = True
+while number_correct < 50 and game_running:
     answer_state = screen.textinput(title=f"Guess the State [{number_correct}/50]", prompt="Type the name of a State").title()
     print(answer_state)
     if answer_state in state_list:
@@ -23,6 +24,13 @@ while number_correct < 50:
         turtle.goto(int(answer.x), int(answer.y))
         turtle.write(f"{answer_state}")
         state_list.remove(answer_state)
+    elif answer_state == "Exit":
+        game_running = False
+        missed_states_dict = {"Missed States": state_list}
+        df = pandas.DataFrame(missed_states_dict)
+        df.to_csv("missed_states.csv")
+
+
 
 
 #screen.exitonclick()
